24460b4911ff5d2bb60b52990462a7a0c608693e
fvillalobos <fvillalobosviveros18@gmail.com>
2024-07-09 21:54:39 -0400

    PreEntrega1Villalobos

---
+++ proyecto_react/src/App.jsx
@@ -0,0 +1,15 @@
+import { NavBar } from "./Components/NavBar";
+import { ItemListContainer } from "./Components/ItemListContainer"; 
+
+function App() {
+  return (
+  <><>
+      <NavBar />
+    </><ItemListContainer greeting= "hola"/></>
+
+  );
+
+  
+}
+
+export default App
---
+++ proyecto_react/src/Components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import Container from 'react-bootstrap/Container';
+
+// eslint-disable-next-line react/prop-types
+export const ItemListContainer = (props) =>
+ // eslint-disable-next-line react/prop-types
+ <Container className='mt-4'><h1>{props.greeting}</h1>
+ </Container>
